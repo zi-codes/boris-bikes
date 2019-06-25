23fa444a82bd290fdf30617d954ee336d731476a
Student <student@Makerss-MacBook-Pro.local>
2019-06-25 16:54:59 +0100

    Add Bike class and .working? method and make rspec test

---
+++ spec/bike_spec.rb
@@ -0,0 +1,4 @@
+require 'Bike'
+describe Bike do
+  it {is_expected.to respond_to(:working?)}
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,4 @@
 require 'DockingStation'
 describe DockingStation do
-  # it { is_expected.to respond_to(:release_bike) }
-  it { respond_to(:release_bike) }
+  it { is_expected.to respond_to(:release_bike) }
 end
